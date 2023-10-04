b7bb2eaf79ad381c88279cb4c62eea4e888f62be
PhillyTechnology <123893565+PhillyTechnology@users.noreply.github.com>
2023-10-04 19:40:44 -0400

    add curl

---
+++ files/scripts/curl.sh
@@ -0,0 +1,7 @@
+echo 'Hello World'
+
+echo 'What is your favorite color?'
+read color
+echo 'Your favorite color is '$color'. What is your favorite number?'
+read number
+echo 'Your favorite number is '$color'.
