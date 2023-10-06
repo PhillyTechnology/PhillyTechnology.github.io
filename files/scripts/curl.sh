750a53c39796674a876d4be4139cbfb89f248e69
PhillyTechnology <123893565+PhillyTechnology@users.noreply.github.com>
2023-10-05 20:16:41 -0400

    remove test curling

--- files/scripts/curl.sh
+++
@@ -1,7 +0,0 @@
-echo 'Hello World'
-
-echo 'What is your favorite color?'
-read color
-echo 'Your favorite color is '$color'. What is your favorite number?'
-read number
-echo 'Your favorite number is '$number.
